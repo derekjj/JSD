014b722e571c83cce00f7af93bb20b16401274b5
Derek J <derek@derekjohnston.ca>
2024-07-20 18:31:47 -0400

    linter

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,7 +85,8 @@ export default {
 			authDomain: process.env.NUXT_ENV_FIREBASE_AUTH_DOMAIN,
 			projectId: process.env.NUXT_ENV_FIREBASE_PROJECT_ID,
 			storageBucket: process.env.NUXT_ENV_FIREBASE_STORAGE_BUCKET,
-			messagingSenderId: process.env.NUXT_ENV_FIREBASE_MESSAGING_SENDER_ID,
+			messagingSenderId:
+				process.env.NUXT_ENV_FIREBASE_MESSAGING_SENDER_ID,
 			appId: process.env.NUXT_ENV_FIREBASE_APP_ID,
 			measurementId: process.env.NUXT_ENV_FIREBASE_MEASUREMENT_ID,
 		},
