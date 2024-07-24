817278fa76f6075d364ac1b75c79a1eae53b6ff4
Derek J <derek@derekjohnston.ca>
2024-07-24 12:54:13 -0400

    update error pages to use custom

--- pages/404.vue
+++ pages/404.vue
@@ -1,6 +1,8 @@
 <template>
 	<div>
 		<h1>404 - Page Not Found</h1>
+		<nuxt-link :to="$route.path">{{ $route.path }}</nuxt-link>
 		<p>The page you are looking for does not exist.</p>
+		<nuxt-link to="/">Home</nuxt-link>
 	</div>
 </template>
--- pages/500.vue
+++ pages/500.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
-		<h1>Page Not Found</h1>
+		<h1>500 - Internal Server Error</h1>
 		<p>The page you are looking for does not exist.</p>
+		<nuxt-link to="/">Home</nuxt-link>
 	</div>
 </template>
