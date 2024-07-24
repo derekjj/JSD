ed4d4dc98c1f08aca004bbd239c33781f56ba059
Derek J <derek@derekjohnston.ca>
2024-07-24 12:33:32 -0400

    handle /projects base route for page reload on deploy

---
+++ pages/projects/index.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { mapGetters } from 'vuex'
+
+export default {
+	computed: {
+		...mapGetters(['jsd']),
+	},
+	watch: {
+		jsd(val) {
+			// handle when direct loading on page from url
+			if (val && val.length > 0) {
+				console.log('jsd', val)
+				this.$router.push('/projects/' + val[0].id)
+			}
+		},
+	},
+	mounted() {
+		// handle when navigating to page from another page
+		if (this.jsd && this.jsd.length > 0) {
+			this.$router.push('/projects/' + this.jsd[0].id)
+		}
+	},
+}
+</script>
